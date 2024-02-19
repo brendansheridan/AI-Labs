23c168e4e51dc24c6f8f87e9ab736529e81c26a7
Brendan Sheridan <bsheridandesign@gmail.com>
2024-02-19 10:58:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
+import os
 
 st.set_page_config(page_title="Document Genie", layout="wide")
 
