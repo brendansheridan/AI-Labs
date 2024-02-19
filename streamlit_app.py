c55adef6d23d425d0d78ad8371d573e194ff5fcc
Brendan Sheridan <bsheridandesign@gmail.com>
2024-02-19 11:06:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
-import PyPDF2
-import google-generativeai
-import langchain
-import chromadb
-import faiss-cpu
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
 import os
 
 st.set_page_config(page_title="Document Genie", layout="wide")
